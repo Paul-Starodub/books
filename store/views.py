c8d018c6d8bc860e999cbcb5aa012a6fdd41359c
Paul Starodub <amorallex@gmail.com>
2023-09-28 21:29:08 +0300

    Updated store/views.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from store.models import Book
+from store.serializers import BooksSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
+
+
+
