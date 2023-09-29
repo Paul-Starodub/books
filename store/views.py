c223989c5bdd9dac766251d7c467f63ee57870a0
Paul Starodub <amorallex@gmail.com>
2023-09-30 00:28:13 +0300

    Updated views.py / models.py for search

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from store.models import Book
 from store.serializers import BooksSerializer
@@ -8,5 +9,6 @@ from store.serializers import BooksSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ["price"]
+    search_fields = ["name", "author_name"]
