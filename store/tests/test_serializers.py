0d939a3dc7c7bebf0ea79a4adb1e8fb00037b7bc
Paul Starodub <amorallex@gmail.com>
2023-09-30 01:03:52 +0300

    Updated tests for search&filtering

--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -6,12 +6,26 @@ from store.serializers import BooksSerializer
 
 class BooksSerializerTestCase(TestCase):
     def test_ok(self) -> None:
-        book_1 = Book.objects.create(name="Test book_1", price="25")
-        book_2 = Book.objects.create(name="Test book_2", price="55")
+        book_1 = Book.objects.create(
+            name="Test book_1", price="25", author_name="name1"
+        )
+        book_2 = Book.objects.create(
+            name="Test book_2", price="55", author_name="name2"
+        )
         data = BooksSerializer([book_1, book_2], many=True).data
         expected_data = [
-            {"id": book_1.id, "name": "Test book_1", "price": "25.00"},
-            {"id": book_2.id, "name": "Test book_2", "price": "55.00"},
+            {
+                "id": book_1.id,
+                "name": "Test book_1",
+                "price": "25.00",
+                "author_name": "name1",
+            },
+            {
+                "id": book_2.id,
+                "name": "Test book_2",
+                "price": "55.00",
+                "author_name": "name2",
+            },
         ]
 
         self.assertEqual(data, expected_data)
