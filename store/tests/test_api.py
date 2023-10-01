4b889f6770d88db11639e5b49c80c808f00ed313
Paul Starodub <amorallex@gmail.com>
2023-10-01 13:49:28 +0300

    Updated test_api.py

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -3,6 +3,7 @@ import json
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
 from rest_framework.test import APITestCase
 
 from store.models import Book
@@ -13,7 +14,10 @@ class BooksApiTestCase(APITestCase):
     def setUp(self) -> None:
         self.user = User.objects.create(username="test username")
         self.book_1 = Book.objects.create(
-            name="Test book_1", price="25", author_name="Author 1"
+            name="Test book_1",
+            price="25",
+            author_name="Author 1",
+            owner=self.user,
         )
         self.book_2 = Book.objects.create(
             name="Test book_2", price="55", author_name="Author 2"
@@ -66,6 +70,7 @@ class BooksApiTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Book.objects.count(), 4)
+        self.assertEqual(Book.objects.last().owner, self.user)
 
     def test_update(self) -> None:
         url = reverse("book-detail", args=(self.book_1.id,))
@@ -93,3 +98,52 @@ class BooksApiTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 2)
         self.assertFalse(Book.objects.filter(id=self.book_1.id).exists())
+
+    def test_update_not_owner(self) -> None:
+        self.user2 = User.objects.create(username="test username2")
+
+        url = reverse("book-detail", args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 575,
+            "author_name": self.book_1.author_name,
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user2)
+        response = self.client.put(
+            url, data=json_data, content_type="application/json"
+        )
+
+        self.assertEqual(
+            response.data,
+            {
+                "detail": ErrorDetail(
+                    string="You do not have permission to perform this action.",
+                    code="permission_denied",
+                )
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.book_1.refresh_from_db()
+        self.assertEqual(self.book_1.price, 25)
+
+    def test_update_not_owner_but_staff(self) -> None:
+        self.user2 = User.objects.create(
+            username="test username2", is_staff=True
+        )
+
+        url = reverse("book-detail", args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 575,
+            "author_name": self.book_1.author_name,
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user2)
+        response = self.client.put(
+            url, data=json_data, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book_1.refresh_from_db()
+        self.assertEqual(self.book_1.price, 575)
