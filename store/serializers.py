0591130463a3153b49c7768f58a0dd86dbddb530
Paul Starodub <amorallex@gmail.com>
2023-10-02 18:07:11 +0300

    Updated serializers.py with annotated_likes via annotate

--- store/serializers.py
+++ store/serializers.py
@@ -5,11 +5,20 @@ from store.models import Book, UserBookRelation
 
 class BooksSerializer(serializers.ModelSerializer):
     likes_count = serializers.SerializerMethodField()
+    annotated_likes = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Book
-        fields = ("id", "name", "price", "author_name", "likes_count")
+        fields = (
+            "id",
+            "name",
+            "price",
+            "author_name",
+            "likes_count",
+            "annotated_likes",
+        )
 
+    #  additional query to db
     def get_likes_count(self, instance: Book) -> UserBookRelation:
         return UserBookRelation.objects.filter(
             book=instance, like=True
