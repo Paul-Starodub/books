f89441b1b98864c6a5db9ad2b618e7cb912098c8
Paul Starodub <amorallex@gmail.com>
2023-10-02 08:05:29 +0300

    Updated views.py for `userbookrelation`

--- store/views.py
+++ store/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.serializers import Serializer
+from rest_framework.viewsets import GenericViewSet
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 from store.permissions import IsOwnerOrStaffOrReadOnly
-from store.serializers import BooksSerializer
+from store.serializers import BooksSerializer, UserBookRelationSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -21,6 +23,19 @@ class BookViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer: Serializer) -> None:
         serializer.save(owner=self.request.user)
+
+
+class UserBookRelationView(mixins.UpdateModelMixin, GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = UserBookRelation.objects.all()
+    serializer_class = UserBookRelationSerializer
+    lookup_field = "book"
+
+    def get_object(self) -> None:
+        obj, _ = UserBookRelation.objects.get_or_create(
+            user=self.request.user, book_id=self.kwargs["book"]
+        )
+        return obj
 
 
 def auth(request: HttpRequest):
