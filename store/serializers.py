4200c1beb94d3801cefc3bf0fc54258d680204e1
Paul Starodub <amorallex@gmail.com>
2023-10-02 20:36:09 +0300

    Updated serializers.py - deleting likes_count

--- store/serializers.py
+++ store/serializers.py
@@ -4,7 +4,6 @@ from store.models import Book, UserBookRelation
 
 
 class BooksSerializer(serializers.ModelSerializer):
-    likes_count = serializers.SerializerMethodField()
     annotated_likes = serializers.IntegerField(read_only=True)
     rating = serializers.DecimalField(
         max_digits=3, decimal_places=2, read_only=True
@@ -17,16 +16,9 @@ class BooksSerializer(serializers.ModelSerializer):
             "name",
             "price",
             "author_name",
-            "likes_count",
             "annotated_likes",
             "rating",
         )
-
-    #  additional query to db
-    def get_likes_count(self, instance: Book) -> UserBookRelation:
-        return UserBookRelation.objects.filter(
-            book=instance, like=True
-        ).count()
 
 
 class UserBookRelationSerializer(serializers.ModelSerializer):
