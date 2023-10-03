a954b3aa9dc4c2ec619400dadf1afb51f03383c6
Paul Starodub <amorallex@gmail.com>
2023-10-03 21:33:35 +0300

    Updated models.py with save

--- store/models.py
+++ store/models.py
@@ -12,6 +12,9 @@ class Book(models.Model):
     readers = models.ManyToManyField(
         User, through="UserBookRelation", related_name="books"
     )
+    rating = models.DecimalField(
+        max_digits=3, decimal_places=2, default=None, null=True
+    )
 
     def __str__(self) -> str:
         return f"Id {self.id}: {self.name}"
@@ -37,3 +40,19 @@ class UserBookRelation(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.username}: {self.book.name}, RATE {self.rate}"
+
+    def save(
+        self,
+        force_insert: bool = False,
+        force_update: bool = False,
+        using: str = None,
+        update_fields: list = None,
+    ) -> None:
+        from store.logic import set_rating
+
+        creating = not self.pk
+        old_rating = self.rate
+        super().save()
+        new_rating = self.rate
+        if old_rating != new_rating or creating:
+            set_rating(self.book)
